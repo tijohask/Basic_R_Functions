dfda6314c4fb1e77b3e6e12ac805fb306f92dd89
Johnny Carlo <timothyjhask@gmail.com>
2017-06-07 00:48:25 -0400

    Contains useful basic functions in R
    Includes declarations (numbers, lists, and factors), and certain math functions.

---
+++ Basic_R_Math.R
@@ -0,0 +1,39 @@
+# numeric declarations
+a = 1
+b <- 2
+3 -> c
+
+# list declarations
+x = c(2,4,6,8)
+y = c(1,2,3,4)
+
+# round to the nearest whole number, up if even
+round(7/4) # 2
+round(6/4) # 2
+round(5/4) # 1
+
+# round up on the fraction
+ceiling(7/4) # 2
+ceiling(5/4) # 2
+
+# round down on the fraction
+floor(7/4) # 1
+floor(5/4) # 1
+
+j
+# 7 / 4 is 1 with a remainder of 3
+
+# division without remainder
+7 %/% 4 # 1
+
+# remainder
+7 %% 4 # 3
+
+# Square root
+sqrt(729) # 27
+
+
+
+
+
+
