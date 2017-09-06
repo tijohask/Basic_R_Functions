96d51480f48e8890dcd7e3675b086c54679fd6d1
Tim Haskins <tijohask@iupui.edu>
2017-09-06 14:09:32 -0400

    Added a file that has basic functions in R.

---
+++ Basic_R_Functions.R
@@ -0,0 +1,11 @@
+# In order to make a vector [which is basically a list], 
+  # put a c in front the parens that contain the elements.
+a = c(1, 2, 3, 4, 5, 8)
+# display individual elements
+a[3]
+# display the whole vector
+a[1:length(a)]
+# display a subset of the vector
+a[2:5]
+
+ls()
